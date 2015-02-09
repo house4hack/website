00a4d4738b1eceeb8872016ae9e10b666d6dae15
Raoul DIFFOUO <diraulo@gmail.com>
2015-02-09 13:25:32 +0200

    start blog integration

--- Gemfile
+++ Gemfile
@@ -1,9 +1,15 @@
 # If you have OpenSSL installed, we recommend updating
 # the following line to use "https"
-source "http://rubygems.org"
+source "https://rubygems.org"
 
 gem "middleman"
+gem "middleman-blog"
 gem "middleman-livereload"
 gem "middleman-deploy"
 gem "bourbon"
 gem "neat"
+
+# For feed.xml.builder
+gem "builder", "~> 3.0"
+
+gem "html2haml"
--- config.rb
+++ config.rb
@@ -1,28 +1,77 @@
 require "extensions/views"
 
 activate :views
-activate :directory_indexes
 
 set :relative_links, true
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
 set :fonts_dir, 'assets/fonts'
-set :layout, 'layouts/application'
+# set :layout, 'layouts/application'
+
 
 configure :development do
   activate :livereload
 end
 
+###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  blog.permalink = "{title}.html"
+  blog.layout = "layouts/blog"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+
+  # blog.default_extension = ".markdown"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+
+activate :directory_indexes
+
 configure :build do
   # Relative assets needed to deploy to Github Pages
   activate :relative_assets
+
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
 end
 
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.method = :git
 end
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
 
 helpers do
   def nav_link(link_text, page_url, options = {})
